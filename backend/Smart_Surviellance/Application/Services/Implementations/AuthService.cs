95a05420eadc436ddd07088fce6549557433e88b
Abdelrahman Atef <3obade2004@gmail.com>
2026-04-14 17:22:43 +0200

    Authentication Api

---
+++ backend/Smart_Surviellance/Application/Services/Implementations/AuthService.cs
@@ -0,0 +1,68 @@
+﻿using Application.Common;
+using Application.Dto;
+using Application.Interfaces;
+using Application.Services.Interfaces;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Services.Implementations
+{
+    public class AuthService : IAuthService
+    {
+
+        private readonly IUserRepository _userRepository;
+        private readonly ITokenService _tokenService;
+        private readonly ISignInService _signInService;
+
+
+        public AuthService(IUserRepository userRepository, ITokenService tokenService, ISignInService signInService)
+        {
+            _userRepository = userRepository;
+            _tokenService = tokenService;
+            _signInService = signInService;
+        }
+
+        public async Task<ApiResponse<AuthResponseDto>> LoginAsync(LoginDto loginDto)
+        {
+            var user = await _userRepository.GetByEmailAsync(loginDto.Email);
+
+            if (user == null)
+                return ApiResponse<AuthResponseDto>.Fail("Invalid credentials");
+
+            var result = await _signInService.CheckPasswordAsync(user, loginDto.Password);
+
+            if (!result)
+                return ApiResponse<AuthResponseDto>.Fail("Invalid credentials");
+
+            var token = _tokenService.CreateToken(user);
+
+            return ApiResponse<AuthResponseDto>.Success(new AuthResponseDto
+            {
+                Email = user.Email,
+                Token = token
+            },"User logged in successfully");
+        }
+
+        public async Task<ApiResponse<AuthResponseDto>> RegisterAsync(RegisterDto registerDto)
+        {
+            if (await _userRepository.IsEmailExistsAsync(registerDto.Email))
+                return ApiResponse<AuthResponseDto>.Fail("Email already exists");
+
+            var user = new User
+            {
+                FirstName = registerDto.FirstName,
+                LastName = registerDto.LastName,
+                Email = registerDto.Email,
+                UserName = registerDto.UserName
+            };
+
+            await _userRepository.CreateUserAsync(user, registerDto.Password);
+
+            
+
+            return ApiResponse<AuthResponseDto>.SuccessNoData("User registered successfully");
+        }
+    }
+}
---
+++ backend/Smart_Surviellance/Smart_Surviellance/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using Application.Dto;
+using Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Smart_Surviellance.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AccountController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            var result = await _authService.LoginAsync(loginDto);
+
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result);
+            }
+
+            return Ok(result);
+
+        }
+
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto registerDto)
+        {
+            var result = await _authService.RegisterAsync(registerDto);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
+    }
+}
