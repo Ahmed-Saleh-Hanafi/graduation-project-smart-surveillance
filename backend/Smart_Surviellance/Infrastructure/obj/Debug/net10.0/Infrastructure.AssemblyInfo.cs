cd9c1914b1b64b8d521f61ba55b1471c5d02634e
Abdelrahman Atef <3obade2004@gmail.com>
2026-04-18 16:43:20 +0200

    Update DbContextFactory

--- backend/Smart_Surviellance/Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
+++ backend/Smart_Surviellance/Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ad984eb0028d27b07fb2ef12cf934e61a33f96f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aef4eaf813c1ab4b0a38326ae12b011e5ea84be6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
