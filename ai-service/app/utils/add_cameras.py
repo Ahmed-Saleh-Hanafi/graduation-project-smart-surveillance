463fb6da684aca74442fcd122e4d27f8f86ad796
Ahmed Saleh Hanafi <eng.ahmed.saleh.2004@gmail.com>
2026-05-09 11:44:53 +0300

    add active cameras to system

--- ai-service/app/utils/add_cameras.py
+++ ai-service/app/utils/add_cameras.py
@@ -1,10 +1,16 @@
+import logging
 from typing import Dict
 from app.config import settings
 
 def add_cameras(cameras:Dict[int, str])->None:
     # validation
     if not cameras:
-        print("[WARNING] No cameras found.")
+        logging.warning('No cameras found')
         return
+    
+    if not isinstance(cameras, dict):
+        raise TypeError('cameras must be dict like {cam_id: rtsp_url}')
+
     settings.CAMERA_SOURCES = cameras
-    print(f"[INFO] Loaded {len(cameras)} cameras.")
+    for cam_id , rtsp_url in settings.CAMERA_SOURCES.items():
+        logging.info(f'Loaded camera: {cam_id} that has rtsp_url: {rtsp_url}')
