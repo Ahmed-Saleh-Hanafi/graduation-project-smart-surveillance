75cabdaf3737cb2de11d4ef3b397ce41f8f1353a
Ahmed Saleh Hanafi <eng.ahmed.saleh.2004@gmail.com>
2026-05-09 12:40:00 +0300

    add active cameras to system

--- ai-service/app/utils/add_cameras.py
+++ ai-service/app/utils/add_cameras.py
@@ -1,4 +1,6 @@
 import logging
+import subprocess
+import cv2
 from typing import Dict
 from app.config import settings
 
@@ -11,6 +13,47 @@ def add_cameras(cameras:Dict[int, str])->None:
     if not isinstance(cameras, dict):
         raise TypeError('cameras must be dict like {cam_id: rtsp_url}')
 
-    settings.CAMERA_SOURCES = cameras
-    for cam_id , rtsp_url in settings.CAMERA_SOURCES.items():
+    success_read_camera = 0
+    for cam_id , rtsp_url in cameras.items():
+        if cam_id == 1: 
+            rtsp_url = 0             # for test
+        if check_rtsp(str(rtsp_url)):
+            logging.warning(f'can not read stream from: {cam_id} that has rtsp_url: {rtsp_url}')
+            continue
+        success_read_camera+= 1
+        settings.CAMERA_SOURCES[cam_id] = rtsp_url
         logging.info(f'Loaded camera: {cam_id} that has rtsp_url: {rtsp_url}')
+    logging.info(f'Loading {success_read_camera} cameras and save them in settings.CAMERA_SOURCES is success')
+
+def check_rtsp(rtsp_url: str)-> bool:
+    if not isinstance(rtsp_url, str):
+        raise TypeError('RTSP URL must be str')
+    
+    cmd = [
+        'ffprobe',
+        '-rtsp_transport',
+        'tcp',
+        '-v',
+        'error',
+        '-show_entries',
+        'stream=codec_type',
+        '-of', 
+        'default=noprint_wrappers=1:nokey=1',
+        rtsp_url
+    ]
+    
+    try:
+        result = subprocess.run(
+            cmd,
+            stdout= subprocess.PIPE,
+            stderr= subprocess.PIPE,
+            timeout=5,
+            text=True
+        )
+        if result.returncode == 0 and 'video' in result.stdout:
+            return True
+        
+        return False
+    
+    except subprocess.TimeoutExpired:
+        return False
