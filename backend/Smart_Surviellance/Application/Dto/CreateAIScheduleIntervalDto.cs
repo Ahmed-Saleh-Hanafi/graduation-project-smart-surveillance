b131f38801147bd717b76e75cac906afd194aebc
Abdo-atef <3obade2004@gmail.com>
2026-05-30 23:38:26 +0300

    Solve errors

---
+++ backend/Smart_Surviellance/Application/Dto/CreateAIScheduleIntervalDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Dto
+{
+    public class CreateAIScheduleIntervalDto
+    {
+
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+
+    }
+}
